1f1c1aee97423374a02f94cf8680210b4e7d0dc5
Olga Tovstaya <olga.tovstaya@yandex.ru>
2022-12-23 20:40:46 +0300

    feat: добавить helmet и limiter

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
+const helmet = require('helmet');
+const apiLimiter = require('./middlewares/apiLimiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const centralErrorHandler = require('./middlewares/centralErrorHandler');
 const routes = require('./routes');
@@ -22,6 +24,7 @@ app.use(cors(
     allowedHeaders: ['Content-Type', 'Authorization'],
   },
 ));
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,13 +32,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect(DATA_BASE_URL);
 
 app.use(requestLogger);
-
+app.use(apiLimiter);
 app.use(routes);
-
 app.use(errorLogger);
-
 app.use(errors());
-
 app.use(centralErrorHandler);
 
 app.listen(PORT, () => {
